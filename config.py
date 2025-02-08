d4f93eb7506fdbbc84b482708880e0fe63ad75f7
RiyoGod <godsxrio@gmail.com>
2025-02-08 14:10:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY" , "AIzaSyA7Fge2TBe8XlQindNtSxPg98u5oV-DHBE")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 MONGO_URI = os.getenv("MONGO_URI")
 
